a5365400e16ea2c50865bf2806504f3e70a48c95
samir-s-hassan <hsamir347@gmail.com>
2024-01-28 11:56:13 -0500

    on challenge 3

--- index.js
+++ index.js
@@ -29,8 +29,12 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
+    for (let i = 0, len = games.length; i < len; i++) {
+        const displayInfo = document.createElement("p");
+        element.classList.add("game-card");
 
-
+      }
+            
         // create a new div element, which will become the game card
 
 
